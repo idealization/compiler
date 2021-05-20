a7c229da66549bacdf075b0c94f09f7c9c1bd1c8
idealization <sanghwaann@gmail.com>
2021-05-20 19:12:53 +0900

    fix error

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -3,33 +3,37 @@ from SLR import *
 
 
 def syntax_analyzer(data):
-    data = data.split()                         # split output of lexical analyzer by blank
+    data = data.split()                         # split output of lexical analyzer by blank and reset to analyze easier
+    del data[-1]
+    data.append('$')
+    data.append('$')
     index = 1                                   # initialize index of starting token
     state_stack = [0]                           # stack that stores states and last element is current state
-    left_substring = [data[index]]              # store left substring that already shifted before
+    left_substring = []                         # store left substring that already shifted before
     while True:
-        if left_substring[-1] == 'OP':          # distinguish +, - and *, / operators
+        if data[index] == 'OP':                 # distinguish +, - and *, / operators
             if data[index + 1] == '+' or data[index + 1] == '-':
-                left_substring[-1] = 'ADDSUB'
+                data[index] = 'ADDSUB'
             else:
-                left_substring[-1] = 'MULTDIV'
+                data[index] = 'MULTDIV'
+        print(state_stack)
+        print(left_substring)
         for action in slr_table.action[state_stack[-1]]:
-            print(action, left_substring)
-            if left_substring[-1] == action[1]:         # find action corresponding to terminal value
+            if data[index] == action[1]:                # find action corresponding to terminal value
                 print("found", action, left_substring)
                 if action[2][0] == 's':                 # shift and goto decision
                     state_stack.append(int(action[2][1:]))
-                    index += 2
                     left_substring.append(data[index])
+                    index += 2
                 elif action[2][0] == 'r':               # reduce decision
-                    if slr_grammar.grammar[int(action[2][1:])][1] == '':
-                        left_substring.insert(-1, slr_grammar.grammar[int(action[2][1:])][0])
-                    else:
-                        for non_terminal in reversed(slr_grammar.grammar[int(action[2][1:])][1].split()):
-                            if non_terminal == left_substring[-1]:
-                                del left_substring[-1]
-                        left_substring.append(slr_grammar.grammar[int(action[2][1:])][0])
-                    state_stack.pop()
+                    for non_terminal in reversed(slr_grammar.grammar[int(action[2][1:])][1].split()):
+                        if non_terminal == left_substring[-1]:
+                            del left_substring[-1]
+                            state_stack.pop()
+                        else:
+                            print(f"There's no goto at state {state_stack[-1]} for {left_substring}")
+                            return
+                    left_substring.append(slr_grammar.grammar[int(action[2][1:])][0])
                     for goto in slr_table.goto[state_stack[-1]]:        # find goto corresponding to non-terminal value
                         if left_substring[-1] == goto[1]:
                             state_stack.append(goto[2])
@@ -39,7 +43,7 @@ def syntax_analyzer(data):
                 break
         else:
             print("reject")
-            print(f"There's no action at state {state_stack[-1]} for {left_substring}")
+            print(f"There's no action at state {state_stack[-1]} for {data[index]}")
             return
 
 
